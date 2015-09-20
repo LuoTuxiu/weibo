fe0e5f7da945ffbe82661707f12f4dcf1cdefb7f
LuoTuxiu <359703836@qq.com>
2015-09-20 09:47:25 +0800

    第一份代码，新添了4个tabBarController

---
+++ 微博大项目/Classes/MessageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageTableViewController.h
+//  微博大项目
+//
+//  Created by 徐磊 on 15/9/20.
+//  Copyright © 2015年 Trbocare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTableViewController : UITableViewController
+
+@end
---
+++ 微博大项目/Classes/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  微博大项目
+//
+//  Created by 徐磊 on 15/9/20.
+//  Copyright © 2015年 Trbocare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
